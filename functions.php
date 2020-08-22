2a77946e7021968df97dab045ce20ac22bb8243d
João Marcos <jaumcj@hotmail.com>
2020-08-21 23:36:58 -0300

    Formulário

--- functions.php
+++ functions.php
@@ -501,4 +501,125 @@ function get_dados($field, $id = null)
 	return get_field($field, $id);
 }
 
+add_action( 'wp_ajax_post_contato', 'filter_post_contato' );
+add_action( 'wp_ajax_nopriv_post_contato', 'filter_post_contato' );
+function filter_post_contato() {
+
+	$response = array();
+	if ($_POST)
+	{
+		$form = $_POST;
+		// if(checkReCaptcha($form))
+		// {
+			send_mail_wp($form['fitlers']);
+			$response['status'] = 'Sucesso';
+			$response['mensagem'] ='Mensagem envida com sucesso';
+		// }
+		// else
+		// {
+		// 	$response['status'] = 'Erro';
+		// 	$response['mensagem'] = 'Erro ao enviar mensagem';
+		// }	
+	}
+	exit( json_encode( $response ) );
+}
+
+function send_mail_wp($form)
+{
+
+	$to = 'jaumcj@gmail.com';
+	// $to = 'joao.marcos@medway.com.br';
+	$subject = 'Contato Site - ('. $form['nome'] .')';
+
+	$headers = array(
+		'Content-Type: text/html; charset=UTF-8'
+	);
+
+	# Topo
+	$body .= '<!DOCTYPE html><html><head></head><body style="background-color: #ebebeb;">';
+
+		$body .= '<table width="730" cellspacing="0" cellpadding="0" height="50"></table>';
+		$body .= '<table width="730" cellspacing="0" cellpadding="0" align="center" style="background-color: #fff;">';
+			$body .= '<tr>';
+				$body .= '<td width="50"></td>';
+				$body .= '<td>';
+
+					$body .= '<table width="630" cellspacing="0" cellpadding="0" align="center" style="font-family: Verdana, Arial, sans-serif; background-color: #fff;">';
+						$body .= '<tr height="30"></tr>';
+						$body .= '<tr>';
+							$body .= '<td width="45"></td>';
+							$body .= '<td width="640">';
+								$actual_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://{$_SERVER['HTTP_HOST']}";
+								$body .= '<img src="' . $actual_link . '/wp-content/uploads/sites/5/2019/08/Screen-Shot-2019-08-05-at-11.10.53.png" width="200" >';
+							$body .= '</td>';
+							$body .= '<td width="45"></td>';
+						$body .= '</tr>';
+						$body .= '<tr height="30"></tr>';
+					$body .= '</table>';
+
+					$body .= '<table width="630" cellspacing="0" cellpadding="0" align="center" style="font-family: Verdana, Arial, sans-serif; background-color: #fff;">';
+						$body .= '<tr><td colspan="2" align="center"><h3>Contato via site</h3></td></tr>';
+						if ($form['nome'] != '') {
+							$body .= '<tr><td style="padding-right:4px; width:24%;"><b>Nome:</b> </td><td>'. $form['nome'] .'</td></tr>';
+						}
+						if ($form['telefone'] != '') {
+							$body .= '<tr><td style="padding-right:4px; width:24%;"><b>Telefone:</b> </td><td>'. $form['telefone'] .'</td></tr>';
+						}
+						if ($form['email'] != '') {
+							$body .= '<tr><td style="padding-right:4px; width:24%;"><b>E-mail:</b> </td><td>'. $form['email'] .'</td></tr>';
+						}
+						if ($form['assunto'] != '') {
+							$body .= '<tr><td style="padding-right:4px; width:24%;"><b>Assunto:</b> </td><td>'. $form['assunto'] .'</td></tr>';
+						}
+						if ($form['mensagem'] != '') {
+							$body .= '<tr><td style="padding-right:4px; width:24%;"><b>Menssage:</b> </td><td>'. $form['mensagem'] .'</td></tr>';
+						}
+
+						if ($form['utm_source'] != '' || $form['utm_medium'] != '' || $form['utm_content'] != '' || $form['utm_campaign'] != '' || $form['utm_term'] != '')
+						{
+							$body .= '<tr><td colspan="2" align="center"><h3>UTM</h3></td></tr>';
+						}
+
+						if ($form['utm_source'] != '') {
+							$body .= '<tr><td style="padding-right:4px; width:24%;"><b>UTM Source:</b> </td><td>'. $form['utm_source'] .'</td></tr>';
+						}
+						if ($form['utm_medium'] != '') {
+							$body .= '<tr><td style="padding-right:4px; width:24%;"><b>UTM Medium:</b> </td><td>'. $form['utm_medium'] .'</td></tr>';
+						}
+						if ($form['utm_content'] != '') {
+							$body .= '<tr><td style="padding-right:4px; width:24%;"><b>UTM Content:</b> </td><td>'. $form['utm_content'] .'</td></tr>';
+						}
+						if ($form['utm_campaign'] != '') {
+							$body .= '<tr><td style="padding-right:4px; width:24%;"><b>UTM Campaign:</b> </td><td>'. $form['utm_campaign'] .'</td></tr>';
+						}
+						if ($form['utm_term'] != '') {
+							$body .= '<tr><td style="padding-right:4px; width:24%;"><b>UTM Term:</b> </td><td>'. $form['utm_term'] .'</td></tr>';
+						}
+
+					$body .= '</table>';
+
+					# Rodapé
+					$body .= '<table width="630" cellspacing="0" cellpadding="0" align="center" style="font-family: Verdana, Arial, sans-serif; background-color: #fff;">';
+						$body .= '<tr height="30"></tr>';
+						$body .= '<tr>';
+							$body .= '<td width="45"></td>';
+							$body .= '<td width="640">';
+								$body .= '<small><b style="color: #000;">Mensagem de e-mail confidencial.</b></small><br>';
+							$body .= '</td>';
+							$body .= '<td width="45"></td>';
+						$body .= '</tr>';
+						$body .= '<tr height="30"></tr>';
+					$body .= '</table>';
+
+				$body .= '</td>';
+				$body .= '<td width="50"></td>';
+			$body .= '</tr>';
+		$body .= '</table>';
+		$body .= '<table width="730" cellspacing="0" cellpadding="0" height="50"></table>';
+
+	$body .= '</body></html>';
+
+	wp_mail( $to, $subject, $body, $headers, null );
+}
+
 ?>
--- src/components/contato/index.php
+++ src/components/contato/index.php
@@ -3,14 +3,30 @@
     <div class="formulario">
       <div class="box">
         <span class="style-font-01">Entre em contato</span>
-        <form action="">
-          <input type="text" placeholder="Nome">
-          <input type="email" placeholder="E-Mail">
-          <input type="text" placeholder="Telefone">
-          <textarea name="assunto" id="" cols="30" rows="10" placeholder="Assunto"></textarea>
-          <button type="submit">
+        <form id="contato">
+          <input id="nome" type="text" placeholder="Nome">
+          <input id="email" type="email" placeholder="E-Mail">
+          <input id="telefone" type="text" placeholder="Telefone">
+          <textarea name="assunto" id="assunto" cols="30" rows="10" placeholder="Assunto"></textarea>
+          <button class="btn-contato">
             <img src="<?php echo get_template_directory_uri(); ?>/dist/imgs/send.svg" alt="">
           </button>
+          <div class="loading">
+            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style="margin: auto; display: block; width: 100px; height: auto;" width="100px" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid">
+              <rect x="17.5" y="30" width="15" height="40" fill="#ffb800">
+                <animate attributeName="y" repeatCount="indefinite" dur="1s" calcMode="spline" keyTimes="0;0.5;1" values="18;30;30" keySplines="0 0.5 0.5 1;0 0.5 0.5 1" begin="-0.2s"></animate>
+                <animate attributeName="height" repeatCount="indefinite" dur="1s" calcMode="spline" keyTimes="0;0.5;1" values="64;40;40" keySplines="0 0.5 0.5 1;0 0.5 0.5 1" begin="-0.2s"></animate>
+              </rect>
+              <rect x="42.5" y="30" width="15" height="40" fill="#ffb800">
+                <animate attributeName="y" repeatCount="indefinite" dur="1s" calcMode="spline" keyTimes="0;0.5;1" values="20.999999999999996;30;30" keySplines="0 0.5 0.5 1;0 0.5 0.5 1" begin="-0.1s"></animate>
+                <animate attributeName="height" repeatCount="indefinite" dur="1s" calcMode="spline" keyTimes="0;0.5;1" values="58.00000000000001;40;40" keySplines="0 0.5 0.5 1;0 0.5 0.5 1" begin="-0.1s"></animate>
+              </rect>
+              <rect x="67.5" y="30" width="15" height="40" fill="#ffb800">
+                <animate attributeName="y" repeatCount="indefinite" dur="1s" calcMode="spline" keyTimes="0;0.5;1" values="20.999999999999996;30;30" keySplines="0 0.5 0.5 1;0 0.5 0.5 1"></animate>
+                <animate attributeName="height" repeatCount="indefinite" dur="1s" calcMode="spline" keyTimes="0;0.5;1" values="58.00000000000001;40;40" keySplines="0 0.5 0.5 1;0 0.5 0.5 1"></animate>
+              </rect>
+            </svg>
+          </div>
         </form>
       </div>
     </div>
