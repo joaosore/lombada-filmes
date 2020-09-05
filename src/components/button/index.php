72bb2f64ffb7442d9e10fc5f1818f730fd849630
João Marcos <jaumcj@hotmail.com>
2020-09-04 22:14:09 -0300

    Blog

--- src/components/button/index.php
+++ src/components/button/index.php
@@ -1,7 +1,7 @@
 <?php
 function button($data) {
   $data = (object) $data;
-  return '<a class="button" href="'.$data->href.'" tagert="'.$data->target.'">
+  return '<a class="button" href="'.$data->url.'" tagert="'.$data->target.'">
             '.$data->title.'
           </a>';
 }
