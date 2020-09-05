badba16218946a55faed6dc2538fd0debfacb1f9
João Marcos <jaumcj@hotmail.com>
2020-09-04 21:53:20 -0300

    Blog

--- src/components/player-mp3/script.js
+++ src/components/player-mp3/script.js
@@ -43,103 +43,103 @@ if (music) {
     },
     false
   );
-}
 
-// returns click as decimal (.77) of the total timelineWidth
-function clickPercent(event) {
-  return (event.clientX - getPosition(timeline)) / timelineWidth;
-}
-
-// Boolean value so that audio position is updated only when the playhead is released
-var onplayhead = false;
-
-// mouseDown EventListener
-function mouseDown() {
-  onplayhead = true;
-  window.addEventListener("mousemove", moveplayhead, true);
-  music.removeEventListener("timeupdate", timeUpdate, false);
-}
-
-// mouseUp EventListener
-// getting input from all mouse clicks
-function mouseUp(event) {
-  if (onplayhead == true) {
-    moveplayhead(event);
-    window.removeEventListener("mousemove", moveplayhead, true);
-    // change current time
-    music.currentTime = duration * clickPercent(event);
-    music.addEventListener("timeupdate", timeUpdate, false);
+  // returns click as decimal (.77) of the total timelineWidth
+  function clickPercent(event) {
+    return (event.clientX - getPosition(timeline)) / timelineWidth;
   }
-  onplayhead = false;
-}
-// mousemove EventListener
-// Moves playhead as user drags
-function moveplayhead(event) {
-  var newMargLeft = event.clientX - getPosition(timeline);
 
-  if (newMargLeft >= 0 && newMargLeft <= timelineWidth) {
-    // playhead.style.marginLeft = newMargLeft + "px";
-    playhead.style.width = newMargLeft + "px";
+  // Boolean value so that audio position is updated only when the playhead is released
+  var onplayhead = false;
+
+  // mouseDown EventListener
+  function mouseDown() {
+    onplayhead = true;
+    window.addEventListener("mousemove", moveplayhead, true);
+    music.removeEventListener("timeupdate", timeUpdate, false);
   }
-  if (newMargLeft < 0) {
-    // playhead.style.marginLeft = "0px";
-    playhead.style.width = "0px";
+
+  // mouseUp EventListener
+  // getting input from all mouse clicks
+  function mouseUp(event) {
+    if (onplayhead == true) {
+      moveplayhead(event);
+      window.removeEventListener("mousemove", moveplayhead, true);
+      // change current time
+      music.currentTime = duration * clickPercent(event);
+      music.addEventListener("timeupdate", timeUpdate, false);
+    }
+    onplayhead = false;
   }
-  if (newMargLeft > timelineWidth) {
-    // playhead.style.marginLeft = timelineWidth + "px";
-    playhead.style.width = timelineWidth + "px";
+  // mousemove EventListener
+  // Moves playhead as user drags
+  function moveplayhead(event) {
+    var newMargLeft = event.clientX - getPosition(timeline);
+
+    if (newMargLeft >= 0 && newMargLeft <= timelineWidth) {
+      // playhead.style.marginLeft = newMargLeft + "px";
+      playhead.style.width = newMargLeft + "px";
+    }
+    if (newMargLeft < 0) {
+      // playhead.style.marginLeft = "0px";
+      playhead.style.width = "0px";
+    }
+    if (newMargLeft > timelineWidth) {
+      // playhead.style.marginLeft = timelineWidth + "px";
+      playhead.style.width = timelineWidth + "px";
+    }
   }
-}
 
-// timeUpdate
-// Synchronizes playhead position with current point in audio
-function timeUpdate() {
-  var playPercent = timelineWidth * (music.currentTime / duration);
-  // playhead.style.marginLeft = playPercent + "px";
-  playhead.style.width = playPercent + "px";
+  // timeUpdate
+  // Synchronizes playhead position with current point in audio
+  function timeUpdate() {
+    var playPercent = timelineWidth * (music.currentTime / duration);
+    // playhead.style.marginLeft = playPercent + "px";
+    playhead.style.width = playPercent + "px";
 
-  tempoAtual.innerText = msToTime(music.currentTime * 1000);
-  tempoFinal.innerText = msToTime(duration * 1000);
+    tempoAtual.innerText = msToTime(music.currentTime * 1000);
+    tempoFinal.innerText = msToTime(duration * 1000);
 
-  if (music.currentTime == duration) {
-    pButton.className = "";
-    pButton.className = "play";
+    if (music.currentTime == duration) {
+      pButton.className = "";
+      pButton.className = "play";
+    }
   }
-}
 
-//Play and Pause
-function play() {
-  console.log("AQUI");
-  // start music
-  if (music.paused) {
-    music.play();
-    // remove play, add pause
-    pButton.className = "";
-    pButton.className = "pause";
-  } else {
-    // pause music
-    music.pause();
-    // remove pause, add play
-    pButton.className = "";
-    pButton.className = "play";
+  //Play and Pause
+  function play() {
+    console.log("AQUI");
+    // start music
+    if (music.paused) {
+      music.play();
+      // remove play, add pause
+      pButton.className = "";
+      pButton.className = "pause";
+    } else {
+      // pause music
+      music.pause();
+      // remove pause, add play
+      pButton.className = "";
+      pButton.className = "play";
+    }
   }
-}
 
-// getPosition
-// Returns elements left position relative to top-left of viewport
-function getPosition(el) {
-  return el.getBoundingClientRect().left;
-}
+  // getPosition
+  // Returns elements left position relative to top-left of viewport
+  function getPosition(el) {
+    return el.getBoundingClientRect().left;
+  }
 
-function msToTime(duration) {
-  var milliseconds = parseInt((duration % 1000) / 100),
-    seconds = Math.floor((duration / 1000) % 60),
-    minutes = Math.floor((duration / (1000 * 60)) % 60),
-    hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
+  function msToTime(duration) {
+    var milliseconds = parseInt((duration % 1000) / 100),
+      seconds = Math.floor((duration / 1000) % 60),
+      minutes = Math.floor((duration / (1000 * 60)) % 60),
+      hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
 
-  hours = hours < 10 ? "0" + hours : hours;
-  minutes = minutes < 10 ? "0" + minutes : minutes;
-  seconds = seconds < 10 ? "0" + seconds : seconds;
+    hours = hours < 10 ? "0" + hours : hours;
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
 
-  return hours + ":" + minutes + ":" + seconds;
+    return hours + ":" + minutes + ":" + seconds;
+  }
 }
